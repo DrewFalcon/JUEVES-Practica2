2deb4e05d1b862aecc1bd926b5ec272a0809e0ae
cm3ndez <85890897+cm3ndez@users.noreply.github.com>
2025-11-06 18:44:50 +0100

    Add files via upload

--- Menu/search.c
+++ Menu/search.c
@@ -1,14 +1,12 @@
 /*
-* Created by roberto on 3/5/21.
-*/
-#include <stdio.h>
+ * Created by roberto on 3/5/21.
+ */
 #include "search.h"
-#define MAX_CHAR 1000
-void    results_search(char * from, char *to,
-                       int * n_choices, char *** choices,
-                       int max_length,
-                       int max_rows)
-   /**here you need to do your query and fill the choices array of strings
+
+#include <stdio.h>
+#define MAX_STRING 1000
+void results_search(char* from, char* to, int* n_choices, char*** choices, int max_length, int max_rows)
+/**here you need to do your query and fill the choices array of strings
  *
  * @param from form field from
  * @param to form field to
@@ -16,57 +14,68 @@ void    results_search(char * from, char *to,
  * @param choices fill this with the actual results
  * @param max_length output win maximum width
  * @param max_rows output win maximum number of rows
-  */
+ */
 {
-    int i=0;
-    int t=0;
-    int count = 0;
-    int z = 0;
-    FILE* f = NULL;
-    char stream[MAX_CHAR];
-    char dummy;
-    /* Query Output*/
-    char *query_result_set = NULL;
+        int i = 0;
+        int t = 0;
+        int count = 0;
+        int z = 0;
+        int j = 0;
+        FILE* f = NULL;
+        char stream[MAX_STRING];
+        char dummy;
+        /* Query Output*/
+        char** query_result_set = NULL;
 
-    /*Abrir fichero con el resultado de la consulta*/
-    f = fopen_choicesn("aux.log", "r");
-    if (f == NULL) {
-      return;
-    }
+        /*Abrir fichero con el resultado de la consulta*/
+        f = fopen("aux.log", "r");
+        if (f == NULL) {
+                return;
+        }
 
-    /*Contar cuantos caracteres tiene el resultado de la consulta*/
-    while (fscanf(f, "%c", &dummy) == 1) {
-      count++;
-    }
-    fclose(f);
+        /*Contar cuantas lineas tiene el resultado de la consulta*/
+        while (fgets(stream, MAX_STRING, f)) {
+                count++;
+        }
+        fclose(f);
 
-    /*Reservar memoria para guardar la solucion a imprimir*/
-    query_result_set = (char*)malloc(sizeof(char) * (count+10));
-    if (query_result_set == NULL) {
-      return;
-    }
+        /*Reservar memoria para guardar la solucion a imprimir*/
+        query_result_set = (char**)malloc(sizeof(char*) * (count));
+        if (query_result_set == NULL) {
+                return;
+        }
 
-    /*Abrir de nuevo el fichero para leer linea a linea*/
-    f = fopen("aux.log", "r");
-    if (f == NULL) {
-      return;
-    }
+        for (i = 0; i < count; i++) {
+                query_result_set[i] = (char*)malloc(max_length);
 
-    /*Ir metiendo linea a linea al string final*/
-    while(fgets(stream, MAX_CHAR -1, f)) {
-      fprintf(stdout, "Linea leida: %s", stream);
-      strcat(query_result_set, stream);
-    }
+                if (query_result_set[i] == NULL) {
+                        for (j = 0; j < i; j++) {
+                                free(query_result_set[j]);
+                        }
+                        free(query_result_set);
+                        return;
+                }
+        }
 
-    *n_choices = sizeof(query_result_set) / sizeof(query_result_set[0]);
+        /*Abrir de nuevo el fichero para leer linea a linea*/
+        f = fopen("aux.log", "r");
+        if (f == NULL) {
+                return;
+        }
 
-    max_rows = MIN(*n_choices, max_rows);
-    for (i = 0 ; i < max_rows ; i++) {
-        t = strlen(query_result_set[i])+1;
-        t = MIN(t, max_length);
-        strncpy((*choices)[i], query_result_set[i], t);
-    }
+        /*Ir metiendo linea a linea al string final*/
+        for (i = 0; fgets(stream, MAX_STRING - 1, f); i++) {
+                strcpy(query_result_set[i], stream);
+        }
 
-    fclose(f);
+        *n_choices = MIN(count, max_rows);
+
+        for (i = 0; i < *n_choices; i++) {
+                t = strlen(query_result_set[i]) + 1;
+                t = MIN(t, max_length);
+                strncpy((*choices)[i], query_result_set[i], t);
+                (*choices)[i][max_length - 1] = '\0';
+        }
+
+        fclose(f);
 }
-
